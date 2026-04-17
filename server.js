4cf0794a8deed5bdfed57d4f5943e7935c1cc80e
Shonnie Lloyd A. Salamera Jr. <76914372+Tooru-kokonoe@users.noreply.github.com>
2026-04-17 14:39:04 +0800

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const studentRoutes = require('./routes/studentRoutes');
-
-app.use(express.json()); // Allows your app to read JSON data
-app.use('/api/students', studentRoutes);
-
-app.listen(3000, () => console.log('Server running on port 3000'));
